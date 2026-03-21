03c1096d21999ec417dc6ad5fd5883a84ac7fbf5
sdkloper <saulsellsmd@gmail.com>
2026-03-20 21:03:55 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -535,11 +535,7 @@ function analyzeDealFromButton(e) {
 
   const btn = e.currentTarget;
 
-// Clean + format numeric values
-  const formatTwo = (val) => {
-    const num = parseFloat(val);
-    if (isNaN(num)) return 0;
-    return Number(num.toFixed(2));
+
   };
    
  // function debugFreq(value) {
