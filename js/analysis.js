bad219f7481c02bffb09f873cb936d4f7ae5e075
sdkloper <saulsellsmd@gmail.com>
2026-04-12 15:40:15 -0400

    Update analysis.js

--- js/analysis.js
+++ js/analysis.js
@@ -176,6 +176,10 @@ async function analyzeProperty() {
 /* ANALYZE Flip BUTTONS */
 /* ============================= */
 
+/* ============================= */
+/* ANALYZE BUTTONS */
+/* ============================= */
+
 function buildAnalyzeURL(type, subject, data) {
 
   const baseURL =
@@ -184,12 +188,12 @@ function buildAnalyzeURL(type, subject, data) {
       : "analyzer.html";
 
   const params = new URLSearchParams({
-    price: getNumericValue("listPrice") || subject.listPrice || 0,
+    price: subject["List Price"] || 0,
     arv: data.arv || 0,
     rent: data.rent || 0,
-    address: encodeURIComponent(
-      document.getElementById("address")?.value || subject.Address || ""
-    )
+    taxes: subject["Taxes"] || 0,
+    hoa: subject["HOA"] || 0,
+    address: subject["Address"] || ""
   });
 
   return `${baseURL}?${params.toString()}`;
