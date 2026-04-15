9e0080ace2f919acd874c06a9e5987e440ebedf0
sdkloper <saulsellsmd@gmail.com>
2026-04-14 21:16:35 -0400

    Update analyzer.js

--- js/analyzer.js
+++ js/analyzer.js
@@ -30,10 +30,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
   if (type === "roiOut") {
     title.innerText = "Return On Investment (ROI)";
-    formula.innerText = "Formula: ROI = Net Profit / Total Investmnt Cost";
-    desc.innerText = "Net Profit: Final Sale Price - Total Investment Cost" 
-    "Total Investment Cost: Purchase price + closing costs + renovation costs + holding costs (taxes, insurance, utilities)." 
-    "It tells you how much money you made for every dollar invested.";
+    formula.innerText = "Formula: ROI = Net Profit / Total Investmnt Cost (Purchase price + closing costs + renovation costs + holding costs (taxes, insurance, utilities).";
+    desc.innerText = "It tells you how much money you made for every dollar invested.";
   }
 
 
