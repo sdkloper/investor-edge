f77a59add86901ada48b0d99628ae8ffac564362
sdkloper <saulsellsmd@gmail.com>
2026-04-19 19:28:25 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -616,7 +616,7 @@ window.addEventListener("click", function(event) {
   }
 });
 
-const LOGIN_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzuGtr2AtmQB9-E0vVxRaS-Jtpgz8anqbHO6LGCxJPGPD3Oom8wV9nFRtdU-HPjPI_x/exec";
+//const LOGIN_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzuGtr2AtmQB9-E0vVxRaS-Jtpgz8anqbHO6LGCxJPGPD3Oom8wV9nFRtdU-HPjPI_x/exec";
 
 function updateLastLogin(userId) {
   fetch(LOGIN_WEBHOOK_URL, {
