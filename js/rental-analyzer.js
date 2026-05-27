f8062be9778ab1e60a85a544c60cd558dfafecde
sdkloper <saulsellsmd@gmail.com>
2026-05-26 20:55:36 -0400

    Update rental-analyzer.js

--- js/rental-analyzer.js
+++ js/rental-analyzer.js
@@ -410,8 +410,18 @@ document.getElementById("wCashflow").innerText = f(cashflow);
 //===================
 //Total Cash Invested 
 //====================
+
+let downPaymentDisplay = loan;
+
+  if (type === "conv") {
+  
+    downPaymentDisplay =
+      purchase * downPct;
+  
+  }
+  
 document.getElementById("tcashInvested").innerText = f(cashInvested);
-document.getElementById("tloan").innerText = f(loan);
+document.getElementById("tloan").innerText = f(downPaymentDisplay);
 document.getElementById("tbuyerClosing").innerText = f(buyerClosing);
 document.getElementById("trehab").innerText = f(rehab);
 
