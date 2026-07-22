e879e00ba3cddffcf91d8a4c99dd3be709b14845
sdkloper <saulsellsmd@gmail.com>
2026-07-22 19:44:18 -0400

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -51,7 +51,7 @@ async function loadDashboard() {
     try {
 
         const response = await fetch(
-            `${WEB_APP_URL2}?action=getSummary`
+            `${WEB_APP_URL2}?action=summary`
         );
 
         if (!response.ok) {
